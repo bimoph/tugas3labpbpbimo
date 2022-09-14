687762cbe1e6ea864fa1351d93f6b26f01b87e42
Bimo Priyohutomo <bimopriyohutomo@Bimos-MacBook-Air.local>
2022-09-14 22:37:28 +0700

    tes commit

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,10 @@
 # TODO: Implement Routings Here
+
+from django.urls import path
+from katalog.views import index
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', index, name='index'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+name = "Bimo Priyohutomo"
+student_id = "2106708444"
+item = CatalogItem(item_name="komputer", item_price=10000000, item_stock=4, description="komputer keren", rating=7, item_url="https://www.tokopedia.com/")
+
+# TODO: Create your views here. 
+def index(request):
+    response = {
+        'name': name,
+        'id': student_id,
+        'item' : [item],
+    }
+    return render(request, 'katalog.html', response)
