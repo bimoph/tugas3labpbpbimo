218890959badcaa2a89cf029dfcda030e4fcb68a
Bimo Priyohutomo <bimopriyohutomo@Bimos-MacBook-Air.local>
2022-09-21 20:19:28 +0700

    2

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class MyWatchList(models.Model):
     watched = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
-    rating = models.IntegerField()
+    rating = models.CharField(max_length=1)
     release_date = models.TextField()
     review = models.TextField()
