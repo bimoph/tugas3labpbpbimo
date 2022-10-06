c403a45c51e27fdc4446c9a1215388c19523ce32
Bimo Priyohutomo <bimopriyohutomo@Bimos-MacBook-Air.local>
2022-10-06 10:40:35 +0700

    tugas 5

--- todolist/forms.py
+++ todolist/forms.py
@@ -2,21 +2,35 @@ from cProfile import label
 from todolist.models import Task
 from django import forms
 
+
 class TaskForm(forms.ModelForm):
-    title = forms.CharField(
-        required=True
-        )
-    deskripsi = forms.Textarea(
-        # required=True,
-        # widget=forms.widgets.Textarea(
-        #     attrs={
-        #         "placeholder": "Masukkan deskripsi",
-        #     }
-        # ),
-        # label="",
-    )
+    # title = forms.CharField(
+    #     required=True
+    #     )
+    # deskripsi = forms.Textarea(
+    #     # required=True,
+    #     # widget=forms.widgets.Textarea(
+    #     #     attrs={
+    #     #         "placeholder": "Masukkan deskripsi",
+    #     #     }
+    #     # ),
+    #     # label="",
+    # )
     
     class Meta:
         model = Task
         fields = ('user', 'date', 'title', 'deskripsi')
         exclude = ['user', 'date']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Masukkan title',
+                'required': 'true'
+                }),
+            'deskripsi': forms.TextInput(attrs={
+                'class': "form-control", 
+                'style': 'max-width: 300px;',
+                'placeholder': 'Masukkan deskripsi'
+                })
+        }
